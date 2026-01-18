332fb7959e42dc707486ec323af1d168780c05c1
Edoni78 <edonaliu8@gmail.com>
2026-01-18 20:38:58 +0100

    ui fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,36 @@ const About = () => {
   return (
     <section className="about" id="about">
       <div className="about__container">
-        {/* LEFT */}
+
+
+         <div className="about__text">
+          <h2>
+            About <span>PixelUp</span>
+          </h2>
+
+          <p>
+            PixelUp is a digital studio focused on creating modern, clean and
+            high-converting websites for businesses that want to stand out
+            online.
+          </p>
+
+          <p>
+            We combine strategy, design and development to deliver fast,
+            reliable and scalable web solutions tailored to your business
+            goals.
+          </p>
+
+          <ul className="about__list">
+            <li>Custom website design</li>
+            <li>Business-focused solutions</li>
+            <li>Fast delivery & clear communication</li>
+            <li>Long-term support</li>
+          </ul>
+        </div>
+
+
+
+
         <div className="about__cards">
           <div className="about__card">
             <h3>Clean Design</h3>
@@ -32,31 +61,8 @@ const About = () => {
           </div>
         </div>
 
-      {/* RIGHT */}
-        <div className="about__text">
-          <h2>
-            About <span>PixelUp</span>
-          </h2>
 
-          <p>
-            PixelUp is a digital studio focused on creating modern, clean and
-            high-converting websites for businesses that want to stand out
-            online.
-          </p>
-
-          <p>
-            We combine strategy, design and development to deliver fast,
-            reliable and scalable web solutions tailored to your business
-            goals.
-          </p>
-
-          <ul className="about__list">
-            <li>Custom website design</li>
-            <li>Business-focused solutions</li>
-            <li>Fast delivery & clear communication</li>
-            <li>Long-term support</li>
-          </ul>
-        </div>
+       
 
 
       </div>
